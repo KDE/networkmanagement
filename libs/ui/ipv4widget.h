e81fd3dba99d78204b87c20682196249bbf9db60
Will Stephenson <wstephenson@kde.org>
2008-09-12 11:57:09 +0000

    Add widgets for the main settings groups to the UI library.

---
+++ libs/ui/ipv4widget.h
@@ -0,0 +1,39 @@
+/*
+Copyright 2008 Will Stephenson <wstephenson@kde.org>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License or (at your option) version 3 or any later version
+accepted by the membership of KDE e.V. (or its successor approved
+by the membership of KDE e.V.), which shall act as a proxy 
+defined in Section 14 of version 3 of the license.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef WIREDWIDGET_H
+#define WIREDWIDGET_H
+
+#include "ui_ipv4.h"
+
+#include "settingwidget.h"
+
+#include "knm_export.h"
+
+class KNM_EXPORT IpV4Widget : public SettingWidget
+{
+public:
+    IpV4Widget(QWidget * parent = 0 );
+    virtual ~IpV4Widget();
+private:
+    Ui_SettingsIp4Config m_ui;
+};
+
+#endif
