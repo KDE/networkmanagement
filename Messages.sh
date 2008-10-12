249be5a603f782061d81b661ef623af677348363
Will Stephenson <wstephenson@kde.org>
2008-10-12 18:11:20 +0000

    Translate the applet

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name '*.ui' -o -name '*.rc'` >> rc.cpp
-$XGETTEXT *.cpp widgets/*.cpp libs/ui/*.cpp libs/storage/*.cpp -o $podir/plasma_applet_networkmanager.pot
+$XGETTEXT *.cpp widgets/*.cpp libs/ui/*.cpp libs/storage/*.cpp applet/*.cpp -o $podir/plasma_applet_networkmanager.pot
 rm -f rc.cpp
