fb4318d3355ab2e86466cd901ccd45056d225768
Sebastian Kügler <sebas@kde.org>
2008-10-28 01:23:01 +0000

    Alignment and spacing in the connection item

--- applet/connectionitem.cpp
+++ applet/connectionitem.cpp
@@ -6,7 +6,7 @@ modify it under the terms of the GNU General Public License as
 published by the Free Software Foundation; either version 2 of
 the License or (at your option) version 3 or any later version
 accepted by the membership of KDE e.V. (or its successor approved
-by the membership of KDE e.V.), which shall act as a proxy 
+by the membership of KDE e.V.), which shall act as a proxy
 defined in Section 14 of version 3 of the license.
 
 This program is distributed in the hope that it will be useful,
@@ -31,18 +31,29 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ConnectionItem::ConnectionItem(RemoteConnection * conn, QGraphicsItem * parent)
 : QGraphicsWidget(parent), m_connection(conn)
 {
+    // painting of a non-active connection (or wifi network)
     // icon on left
+    int rowHeight = 24;
     m_layout = new QGraphicsGridLayout(this);
+    // First and third colunm are fixed width for the icons
+    m_layout->setColumnFixedWidth(0, rowHeight);
+    m_layout->setColumnPreferredWidth(1, 200);
+    m_layout->setColumnFixedWidth(2, rowHeight);
+    // tighten
+    m_layout->setColumnSpacing(0, 0);
+    m_layout->setColumnSpacing(1, 0);
+    m_layout->setColumnSpacing(2, 0);
+
     m_icon = new Plasma::Icon(this);
     m_icon->setIcon("network-server");
-    m_icon->setMinimumHeight(32);
-    m_icon->setMaximumHeight(32);
+    m_icon->setMinimumHeight(rowHeight);
+    m_icon->setMaximumHeight(rowHeight);
     m_connectionNameLabel = new Plasma::Label(this);
-    m_connectionNameLabel->setText(conn->id());
+    m_connectionNameLabel->setText("Connection:" + conn->id());
     m_connectButton = new Plasma::Icon(this);
     m_connectButton->setIcon("media-playback-start");
-    m_connectButton->setMinimumHeight(32);
-    m_connectButton->setMaximumHeight(32);
+    m_connectButton->setMinimumHeight(rowHeight);
+    m_connectButton->setMaximumHeight(rowHeight);
 
     m_layout->addItem(m_icon, 0, 0, 1, 1 );
     m_layout->addItem(m_connectionNameLabel, 0, 1, 1, 1);
@@ -62,6 +73,7 @@ RemoteConnection * ConnectionItem::connection() const
 
 void ConnectionItem::emitClicked()
 {
+    kDebug() << "clicked";
     emit clicked(this);
 }
 // vim: sw=4 sts=4 et tw=100
