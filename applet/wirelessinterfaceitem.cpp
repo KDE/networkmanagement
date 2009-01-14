095cda540ea40d0392e41a37f107e9d3a8073dda
Laurent Montel <montel@kde.org>
2009-01-14 12:27:22 +0000

    Add missing i18n

--- applet/wirelessinterfaceitem.cpp
+++ applet/wirelessinterfaceitem.cpp
@@ -173,7 +173,7 @@ void WirelessInterfaceItem::setConnectionInfo()
                 m_connectionInfoIcon->setIcon("object-locked");
             }
         } else {
-            m_connectionInfoLabel->setText("No active connection");
+            m_connectionInfoLabel->setText(i18n("No active connection"));
         }
         m_connectionInfoIcon->show();
     } else {
