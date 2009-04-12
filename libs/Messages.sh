435c889a9a9c0d4140d377178ee82c68594926b2
Andrew Coles <andrew.i.coles@googlemail.com>
2009-04-12 16:39:25 +0000

    SVN_SILENT
    Fetch strings from storage .kcfg file.

--- libs/Messages.sh
+++ libs/Messages.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 $EXTRACTRC `find ui -name '*.ui'` >> rc.cpp
+$EXTRACTRC `find storage -name '*.kcfg'` >> rc.cpp
 $XGETTEXT `find ui -name '*.cpp'` rc.cpp -o $podir/libknmui.pot
 rm -f rc.cpp
