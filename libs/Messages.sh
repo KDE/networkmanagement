7667108bb8ad009d10ef0cf988ea02205f635752
Albert Astals Cid <tsdgeos@terra.es>
2009-06-08 22:30:10 +0000

    storage disappeared, all kcfg files are in this other folder now

--- libs/Messages.sh
+++ libs/Messages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 $EXTRACTRC `find ui -name '*.ui'` >> rc.cpp
-$EXTRACTRC `find storage -name '*.kcfg'` >> rc.cpp
+$EXTRACTRC `find internals -name '*.kcfg'` >> rc.cpp
 $XGETTEXT `find ui -name '*.cpp'` rc.cpp -o $podir/libknmui.pot
 rm -f rc.cpp
