9d5d972e86eca939d6d7352d9524c464065efa22
Will Stephenson <wstephenson@kde.org>
2009-06-21 09:07:33 +0000

    Clean up include guards for new class name and namespace

--- libs/internals/activatable.h
+++ libs/internals/activatable.h
@@ -19,8 +19,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef KNM_EXTERNALS_CONNECTABLE_H
-#define KNM_EXTERNALS_CONNECTABLE_H
+#ifndef KNM_INTERNALS_ACTIVATABLE_H
+#define KNM_INTERNALS_ACTIVATABLE_H
 
 #include "knm_export.h"
 
