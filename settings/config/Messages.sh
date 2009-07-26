553301c708794762cdb6d7cd33273be419b802bd
Albert Astals Cid <tsdgeos@terra.es>
2009-07-26 10:40:25 +0000

    There's no ../schemas anymore CCMAIL: wstephenson@kde.org

--- settings/config/Messages.sh
+++ settings/config/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name '*.ui' -o -name '*.rc'` `find ../schemas -name "*.kcfg"` >> rc.cpp
+$EXTRACTRC `find . -name '*.ui' -o -name '*.rc'` >> rc.cpp
 $XGETTEXT `find . ../configshell -name "*.cpp"` -o $podir/kcm_networkmanagement.pot
 rm -f rc.cpp
