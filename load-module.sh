497679e875fc9c56adf431ba0d312d6d912121b4
Sebastian Kügler <sebas@kde.org>
2009-11-06 21:19:28 +0000

    move those out of the way, they're in tools/ now

--- load-module.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Shortcut for loading the networkmanagement kded module manually
-
-qdbus org.kde.kded /kded loadModule networkmanagement
--- unload-module.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Shortcut for unloading the networkmanagement kded module manually
-
-qdbus org.kde.kded /kded unloadModule networkmanagement
