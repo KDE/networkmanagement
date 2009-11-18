0e2bd5faad6649fd643c7b86f2abf2adcdd24e13
Will Stephenson <wstephenson@kde.org>
2009-11-18 13:40:28 +0000

    Remove commented out cruft.
    Sebas, one day I'm going to come round to your place and ask your mother
    what it was like cleaning up after you as a child :P. (Yes, I have just
    triaged like 400 networkmanagement bug reports and am in fine humour)
    CCMAIL: sebas@kde.org

--- libs/internals/uiutils.cpp
+++ libs/internals/uiutils.cpp
@@ -36,7 +36,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Qt
 #include <QSizeF>
 
-//#include "remoteinterfaceconnection.h"
 #include "knmserviceprefs.h"
 
 QString UiUtils::interfaceTypeLabel(const Solid::Control::NetworkInterface::Type type)
