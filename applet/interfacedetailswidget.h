79c3657f02e5838a1e40a9d74010c571ae4569bd
Alexander Naumov <posix.ru@gmail.com>
2010-04-26 09:46:27 +0000

    svn_test

--- applet/interfacedetailswidget.h
+++ applet/interfacedetailswidget.h
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define INTERFACEDETAILSWIDGET_H
 
 #include <QGraphicsWidget>
+#include <QGraphicsLinearLayout>
 #include <solid/control/networkinterface.h>
 #include <Plasma/Label>
 
