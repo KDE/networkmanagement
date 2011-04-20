aff5a20083622b17dc7a1d2a1924417d1777c1f2
Lamarque V. Souza <lamarque@gmail.com>
2011-04-20 00:11:29 -0300

    Just to inform anybody listening that connections have changed.

--- backends/NetworkManager/nmdbussettingsconnectionprovider.cpp
+++ backends/NetworkManager/nmdbussettingsconnectionprovider.cpp
@@ -202,6 +202,7 @@ void NMDBusSettingsConnectionProvider::clearConnections()
     // Just to make sure d->connections is really clear.
     d->connections.clear();
     d->uuidToPath.clear();
+    emit connectionsChanged();
 }
 
 void NMDBusSettingsConnectionProvider::handleAdd(Knm::Activatable * added)
