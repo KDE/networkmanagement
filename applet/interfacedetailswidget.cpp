67ec5e073ddcab120d0442d40354ab05ca243daa
Lamarque V. Souza <lamarque@gmail.com>
2011-04-19 23:24:50 -0300

    Add the "Enable" information detail again since it is important when using NM-0.8.2. Hardware wwan (AKA mobile broadband) is enabled only when modem is enabled. (cherry picked from commit 3d222bc8c917b2d01342f3998b4989d04a0c1a22)

--- applet/interfacedetailswidget.cpp
+++ applet/interfacedetailswidget.cpp
@@ -289,6 +289,10 @@ void InterfaceDetailsWidget::showDetails(bool reset)
 
         if (giface) {
             info += QString(format)
+                           .arg(i18nc("interface details", "Enabled"))
+                           .arg(details->enabled ? i18n("Yes") : i18n("No"));
+
+            info += QString(format)
                            .arg(i18nc("interface details", "Operator"))
                            .arg(details->registrationInfo.operatorName);
 
@@ -308,10 +312,6 @@ void InterfaceDetailsWidget::showDetails(bool reset)
             info += QString(format)
                            .arg(i18nc("interface details", "Allowed Mode"))
                            .arg(Solid::Control::ModemInterface::convertAllowedModeToString(details->allowedMode));
-
-            info += QString(format)
-                           .arg(i18nc("interface details", "Enabled"))
-                           .arg(details->enabled ? i18n("Yes") : i18n("No"));
 
             info += QString(format)
                            .arg(i18nc("interface details", "UDI"))
