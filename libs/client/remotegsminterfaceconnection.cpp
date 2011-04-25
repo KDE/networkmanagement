2e1e68761e8c81cae2de36746f86b6007e47f6b6
Lamarque V. Souza <lamarque@gmail.com>
2011-04-25 17:11:52 -0300

    Reverting 55282b078d85c49f872c424f22905119444e0520 (make it compile without modemmanager) because the the file libs/client/remotegsminterfaceconnection.cpp belongs to the ModemManager patch, so it does not exist when COMPILE_MODEM_MANAGER_SUPPORT is not defined.

--- libs/client/remotegsminterfaceconnection.cpp
+++ libs/client/remotegsminterfaceconnection.cpp
@@ -43,15 +43,11 @@ int RemoteGsmInterfaceConnection::getAccessTechnology() const
 {
     Q_D(const RemoteGsmInterfaceConnection);
     QDBusReply<int> reply = d->gsmInterfaceConnectionIface->getAccessTechnology();
-#ifdef COMPILE_MODEM_MANAGER_SUPPORT
     if (reply.isValid()) {
         return reply.value();
     } else {
         return Solid::Control::ModemInterface::UnknownTechnology;
     }
-#else
-    return reply.value();
-#endif
 }
 
 int RemoteGsmInterfaceConnection::getSignalQuality() const
