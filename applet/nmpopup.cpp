6da7ce00125bde9aed97c7c919b2938151fa9e34
Christoph Feck <christoph@maxiom.de>
2011-05-31 12:27:46 +0200

    SVN_SILENT compile

--- applet/nmpopup.cpp
+++ applet/nmpopup.cpp
@@ -27,6 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QGraphicsGridLayout>
 #include <QTimer>
 #include <QDBusConnection>
+#include <QDBusInterface>
 
 // KDE
 #include <KDebug>
