48d88b23542117c7a0d745e8b4fd1793fc21a81c
Lamarque V. Souza <lamarque@gmail.com>
2011-06-11 23:10:16 -0300

    Ok, this one fix it for sure.
    CCBUG: 275434

--- applet/nmpopup.cpp
+++ applet/nmpopup.cpp
@@ -260,11 +260,30 @@ void NMPopup::init()
     adjustSize();
 }
 
-static int versionToInt(QString version)
+static int compareVersions(const QString & version1, const QString & version2)
 {
-    QStringList v = version.split('.');
-    if (v.size() > 2) {
-        return KDE_MAKE_VERSION(QString(v[0]).toInt(), QString(v[1]).toInt(), QString(v[2]).toInt());
+    QStringList sl1 = version1.split('.');
+    QStringList sl2 = version2.split('.');
+
+    if (sl1.size() > 2 && sl2.size() > 2) {
+        int v1[3] = { QString(sl1[0]).toInt(), QString(sl1[1]).toInt(), QString(sl1[2]).toInt() };
+        int v2[3] = { QString(sl2[0]).toInt(), QString(sl2[1]).toInt(), QString(sl2[2]).toInt() };
+
+        if (v1[0] > v2[0]) {
+            return 1;
+        } else if (v1[0] < v2[0]) {
+            return -1;
+        } else if (v1[1] > v2[1]) {
+            return 1;
+        } else if (v1[1] < v2[1]) {
+            return -1;
+        } else if (v1[2] > v2[2]) {
+            return 1;
+        } else if (v1[2] < v2[2]) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
     return 0;
 }
@@ -297,10 +316,7 @@ void NMPopup::readConfig()
 
     QString version = Solid::Control::NetworkManagerNm09::version();
     if (!version.isEmpty()) {
-        int nmVersion = versionToInt(version);
-        int nmMinimumVersion = versionToInt(QString(MINIMUM_NM_VERSION_REQUIRED));
-
-        if (nmVersion < nmMinimumVersion) {
+        if (compareVersions(version, QString(MINIMUM_NM_VERSION_REQUIRED)) < 0) {
             Plasma::Label * warning = new Plasma::Label(this);
             warning->setText(i18nc("Warning about wrong NetworkManager version", "We need NetworkManager version >= %1 to work, found %2", QString(MINIMUM_NM_VERSION_REQUIRED), version));
             m_interfaceLayout->addItem(warning);
@@ -498,11 +514,11 @@ void NMPopup::networkingEnabledToggled(bool checked)
 kDebug() << "Wireless hardware enabled ==" << Solid::Control::NetworkManagerNm09::isWirelessHardwareEnabled();
 
 #if KDE_IS_VERSION(4,6,2)
-    m_wifiCheckBox->setEnabled(Solid::Control::NetworkManagerNm09::isWirelessHardwareEnabled());
+    m_wifiCheckBox->setEnabled(Solid::Control::NetworkManagerNm09::isWirelessHardwareEnabled() || Solid::Control::NetworkManagerNm09::isWirelessEnabled());
 #endif
 
     m_wwanCheckBox->setChecked(Solid::Control::NetworkManagerNm09::isWwanEnabled());
-    m_wwanCheckBox->setEnabled(Solid::Control::NetworkManagerNm09::isWwanHardwareEnabled());
+    m_wwanCheckBox->setEnabled(Solid::Control::NetworkManagerNm09::isWwanHardwareEnabled() || Solid::Control::NetworkManagerNm09::isWwanEnabled());
 
     updateHasWireless(checked);
 }
