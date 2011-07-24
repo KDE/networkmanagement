KDE/networkmanagement

commit 1a554ce844d74b34716c8923bf40da64f1454365

Send an error DBus reply to NM if secrets retrieval fails
Currently, we just send an empty settings map. However, at least
for VPN connections, this doesn't cause tem to fail, but they are
stuck in activating state, which happens for example if the user
cancels the auth dialog. Sending an error reply to NM will cause
the connection to fail immediately.

libs/service/secretstorage.cpp
@@ -170,16 +170,16 @@ void SecretStorage::walletOpenedForRead(bool success)
                                 if (!needSecretsList.isEmpty() && (pair.second & AllowInteraction || pair.second & RequestNew)) {
                                     askUser(con, pair.first, needSecretsList);
                                 } else {
-                                    emit connectionRead(con, pair.first);
+                                    emit connectionRead(con, pair.first, false);
                                 }
                             } else {
-                                emit connectionRead(con, pair.first);
+                                emit connectionRead(con, pair.first, false);
                             }
                             break;
                         }
                     }
                     if (!settingsFound) {
-                        emit connectionRead(con, pair.first);
+                        emit connectionRead(con, pair.first, true);
                     }
                     i.remove();
                 }
@@ -196,7 +196,7 @@ void SecretStorage::walletOpenedForRead(bool success)
                 if (i.next().key() != con->uuid())
                     continue;
                 QPair<QString,GetSecretsFlags> pair = i.value();
-                emit connectionRead(con, pair.first);
+                emit connectionRead(con, pair.first, true);
                 i.remove();
             }
          }
@@ -262,10 +262,10 @@ void SecretStorage::loadSecrets(Knm::Connection *con, const QString &name, GetSe
             if (!needSecretsList.isEmpty() && (flags & AllowInteraction || flags & RequestNew)) {
                 askUser(con, name, needSecretsList);
             } else {
-                emit connectionRead(con, name);
+                emit connectionRead(con, name, false);
             }
         } else {
-            emit connectionRead(con, name);
+            emit connectionRead(con, name, false);
         }
     } else if (d->storageMode == Secure && !(flags & RequestNew)) {
         kDebug() << "opening wallet...";
@@ -277,7 +277,7 @@ void SecretStorage::loadSecrets(Knm::Connection *con, const QString &name, GetSe
             QPair<QString,GetSecretsFlags> pair(name, flags);
             d->settingsToRead.insert(uuid, pair);
         } else {
-            emit connectionRead(con, name);
+            emit connectionRead(con, name, true);
         }
     }
 }
@@ -308,7 +308,10 @@ KSharedConfig::Ptr SecretStorage::secretsFileForUuid(const QString & uuid)
 void SecretStorage::gotSecrets(KJob *job)
 {
     ConnectionSecretsJob * csj = static_cast<ConnectionSecretsJob*>(job);
-    emit connectionRead(csj->connection(), csj->settingName());
+    bool failed = true;
+    if (csj->error() == ConnectionSecretsJob::EnumError::NoError)
+        failed = false;
+    emit connectionRead(csj->connection(), csj->settingName(), failed);
 }
 
 void SecretStorage::switchStorage(SecretStorageMode oldMode, SecretStorageMode newMode)

backends/NetworkManager/nmdbussecretagent.cpp
@@ -151,17 +151,21 @@ void NMDBusSecretAgent::deleteSavedConnection(Knm::Connection *con)
 }
 
 
-void NMDBusSecretAgent::secretsReady(Knm::Connection *con, const QString &name)
+void NMDBusSecretAgent::secretsReady(Knm::Connection *con, const QString &name, bool failed)
 {
     Q_D(NMDBusSecretAgent);
     QPair<QString, QDBusMessage> pair = d->connectionsToRead.take(con->uuid() + name);
     if (d->objectPaths.removeOne(pair.first + name)) {
-        ConnectionDbus condbus(con);
-        QVariantMapMap secrets = condbus.toDbusSecretsMap(name);
-
-        QDBusMessage reply = pair.second.createReply();
-        QVariant arg = QVariant::fromValue(secrets);
-        reply << arg;
+        QDBusMessage reply;
+        if (failed) {
+            reply = pair.second.createErrorReply(QDBusError::Failed, QString());
+        } else {
+            ConnectionDbus condbus(con);
+            QVariantMapMap secrets = condbus.toDbusSecretsMap(name);
+            reply = pair.second.createReply();
+            QVariant arg = QVariant::fromValue(secrets);
+            reply << arg;
+        }
         QDBusConnection::systemBus().send(reply);
         delete con;
     }
@@ -177,6 +181,6 @@ void NMDBusSecretAgent::registerSecretsProvider(SecretsProvider * provider)
 {
     Q_D(NMDBusSecretAgent);
     d->secretsProvider = provider;
-    connect(d->secretsProvider,SIGNAL(connectionRead(Knm::Connection *, const QString&)),SLOT(secretsReady(Knm::Connection*, const QString&)));
+    connect(d->secretsProvider,SIGNAL(connectionRead(Knm::Connection *, const QString&, bool)),SLOT(secretsReady(Knm::Connection*, const QString&, bool)));
     connect(d->secretsProvider,SIGNAL(connectionSaved(Knm::Connection *)),SLOT(deleteSavedConnection(Knm::Connection *)));
 }

backends/NetworkManager/nmdbussecretagent.h
@@ -58,7 +58,7 @@ private:
     void loadSecrets(Knm::Secrets*);
 
 private Q_SLOTS:
-    void secretsReady(Knm::Connection *, const QString &);
+    void secretsReady(Knm::Connection *, const QString &, bool);
     void deleteSavedConnection(Knm::Connection *);
     void registerAgent();
 };

libs/service/secretsprovider.h
@@ -51,7 +51,7 @@ class KNM_EXPORT SecretsProvider : public QObject
         virtual void deleteSecrets(Knm::Connection *) = 0;
     Q_SIGNALS:
         void connectionSaved(Knm::Connection *);
-        void connectionRead(Knm::Connection *, const QString&);
+        void connectionRead(Knm::Connection *, const QString&, bool);
 };
 
 #endif // SECRETSPROVIDER_H

commit 0a1892136324263af321a2a93f965088c0947bba

Fix NM-0.8's connection importer which was broken by commit 1a554ce844d74b34716c8923bf40da64f1454365.

kded/nm08connections.cpp
@@ -56,7 +56,7 @@ Nm08Connections::Nm08Connections(SecretStorage * secretStorage, NMDBusSettingsCo
     m_connectionsToImport = KNetworkManagerServicePrefs::self()->connections();
     kDebug() << "Connections to import:" << m_connectionsToImport;
 
-    connect(m_secretStorage, SIGNAL(connectionRead(Knm::Connection *, const QString&)), SLOT(gotSecrets(Knm::Connection *)));
+    connect(m_secretStorage, SIGNAL(connectionRead(Knm::Connection *, const QString&, bool)), SLOT(gotSecrets(Knm::Connection *, const QString&, bool)));
     connect(m_nmDBusConnectionProvider, SIGNAL(addConnectionCompleted(bool, const QString &)), SLOT(importNextNm08Connection()));
 }
 
@@ -88,8 +88,8 @@ void Nm08Connections::importNextNm08Connection()
             }
             qDeleteAll(m_connectionsToDelete);
             m_connectionsToDelete.clear();
-            deleteLater();
         }
+        deleteLater();
         return;
     }
 
@@ -144,7 +144,7 @@ END:
     QTimer::singleShot(0, this, SLOT(importNextNm08Connection()));
 }
 
-void Nm08Connections::gotSecrets(Knm::Connection * connection)
+void Nm08Connections::gotSecrets(Knm::Connection * connection, const QString & settingName, bool ok)
 {
     if (!m_connectionsToDelete.contains(connection) || m_connectionsBeingAdded.contains(connection)) {
         return;

kded/nm08connections.h
@@ -39,7 +39,7 @@ public Q_SLOTS:
     void importNextNm08Connection();
 
 private Q_SLOTS:
-    void gotSecrets(Knm::Connection *);
+    void gotSecrets(Knm::Connection *, const QString&, bool);
 
 private:
     Knm::SettingPersistence * persistenceFor(Knm::Setting * setting, KSharedConfig::Ptr config);

libs/service/secretstorage.cpp
@@ -96,7 +96,6 @@ void SecretStorage::saveSecrets(Knm::Connection *con)
 void SecretStorage::walletOpenedForWrite(bool success)
 {
     Q_D(SecretStorage);
-    kDebug();
     if (success) {
         KWallet::Wallet * wallet = static_cast<KWallet::Wallet*>(sender());
         if (wallet->isOpen()) {
@@ -140,6 +139,7 @@ void SecretStorage::walletOpenedForWrite(bool success)
 void SecretStorage::walletOpenedForRead(bool success)
 {
     Q_D(SecretStorage);
+    kDebug();
     bool retrievalSuccessful = true;
     if (success) {
         KWallet::Wallet * wallet = static_cast<KWallet::Wallet*>(sender());