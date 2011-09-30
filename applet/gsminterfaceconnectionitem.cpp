c74523ffb27657ab270eb9a55cdaa0df0065f1cc
Lamarque V. Souza <lamarque@gmail.com>
2011-09-30 12:27:29 -0300

    Make defaultroute overlay icon really appear over the network icon in the network list.

--- applet/gsminterfaceconnectionitem.cpp
+++ applet/gsminterfaceconnectionitem.cpp
@@ -73,6 +73,8 @@ void GsmInterfaceConnectionItem::setupItem()
     // icon on the left
     m_connectButton = new Plasma::IconWidget(this);
     m_connectButton->setMaximumWidth(maxConnectionNameWidth);
+    // to make default route overlay really be over the connection's icon.
+    m_connectButton->setFlags(ItemStacksBehindParent);
     m_connectButton->setAcceptsHoverEvents(false);
     RemoteGsmInterfaceConnection * remote = qobject_cast<RemoteGsmInterfaceConnection*>(m_activatable);
     if (remote) {
