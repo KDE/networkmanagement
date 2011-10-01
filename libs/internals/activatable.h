KDE/networkmanagement

commit 53c55be9b7a104975881334459c0f3a64cf1bfb2

Improve connection sorting
Previously, sorting was done by the plasmoid in a very simple way. newly
added activatables were not being sorted at all. But since we already
have a sorted list on the session bus which resorts every time an activatable
is added, we can just use that.

libs/service/sortedactivatablelist.cpp
@@ -39,6 +39,7 @@ class SortedActivatableListPrivate : public ActivatableListPrivate
 {
 public:
     Solid::Control::NetworkInterfaceNm09::Types types;
+    QHash<Solid::Control::NetworkInterfaceNm09::Type, int> solidTypesToOrder;
 };
 
 // sorting activatables
@@ -56,7 +57,7 @@ public:
 // QAction::addAction/removeAction
 // resort everything or just find correct place for changed item?
 //   (save pointer to last active connection?)
-// make lists of 
+// make lists of
 // k
 
 bool activatableLessThan(const Knm::Activatable * first, const Knm::Activatable * second);
@@ -76,12 +77,19 @@ int compareSsid(const Knm::WirelessObject * first, const Knm::WirelessObject * s
 /* SortedActivatableList */
 
 SortedActivatableList::WirelessSortPolicy SortedActivatableList::s_wirelessSortPolicy = SortedActivatableList::WirelessSortByStrength;
+QHash<Solid::Control::NetworkInterfaceNm09::Types, int> SortedActivatableList::s_solidTypesToOrder = QHash<Solid::Control::NetworkInterfaceNm09::Types, int>();
 
 SortedActivatableList::SortedActivatableList(Solid::Control::NetworkInterfaceNm09::Types types, QObject * parent)
     : ActivatableList(*new SortedActivatableListPrivate, parent)
 {
     Q_D(SortedActivatableList);
     d->types = types;
+    s_solidTypesToOrder.insert(Solid::Control::NetworkInterfaceNm09::Wifi, 0);
+    s_solidTypesToOrder.insert(Solid::Control::NetworkInterfaceNm09::Wimax, 1);
+    s_solidTypesToOrder.insert(Solid::Control::NetworkInterfaceNm09::Bluetooth, 2);
+    s_solidTypesToOrder.insert(Solid::Control::NetworkInterfaceNm09::Ethernet, 3);
+    s_solidTypesToOrder.insert(Solid::Control::NetworkInterfaceNm09::Modem, 4);
+    s_solidTypesToOrder.insert(Solid::Control::NetworkInterfaceNm09::OlpcMesh, 5);
 }
 
 void SortedActivatableList::handleAdd(Knm::Activatable * activatable)
@@ -247,16 +255,8 @@ int compareDevices(const Knm::Activatable * first, const Knm::Activatable * seco
         return 0;
     }
 
-    // VPN workaround for not having a real device
-    uint firstDevWeight = 0, secondDevWeight = 0;
-    if (first->activatableType() == Knm::Activatable::VpnInterfaceConnection) {
-        firstDevWeight = 1;
-    }
-    if (second->activatableType() == Knm::Activatable::VpnInterfaceConnection) {
-        secondDevWeight = 1;
-    }
-    if (firstDevWeight || secondDevWeight) {
-        return firstDevWeight - secondDevWeight;
+    if (first->activatableType() == Knm::Activatable::VpnInterfaceConnection || second->activatableType() == Knm::Activatable::VpnInterfaceConnection || first->activatableType() == Knm::Activatable::WirelessNetwork || second->activatableType() == Knm::Activatable::WirelessNetwork || first->activatableType() == Knm::Activatable::UnconfiguredInterface || second->activatableType() == Knm::Activatable::UnconfiguredInterface) {
+        return 0;
     }
 
     // not VPN, therefore use the real logic
@@ -267,7 +267,7 @@ int compareDevices(const Knm::Activatable * first, const Knm::Activatable * seco
         if (firstIface->type() == secondIface->type()) {
             return firstIface->interfaceName().compare(secondIface->interfaceName());
         } else {
-            return (int)firstIface->type() - (int)secondIface->type();
+            return SortedActivatableList::s_solidTypesToOrder.value(firstIface->type()) - SortedActivatableList::s_solidTypesToOrder.value(secondIface->type());
         }
     }
     return 0;

libs/service/wirelessnetworkinterfaceactivatableprovider.cpp
@@ -170,7 +170,7 @@ void WirelessNetworkInterfaceActivatableProvider::handleAdd(Knm::Connection * ad
                     d->activatables.insert(addedConnection->uuid().toString(), ifaceConnection);
                     d->activatableList->addActivatable(ifaceConnection);
                 }
-                else if (!ourHiddenWicFound) {
+                /*else if (!ourHiddenWicFound) {
                     // create a HiddenWirelessInterfaceConnection for this connection as well
                     // this allows adhoc and hidden wireless networks' connections to be activated
                     // see HiddenWIC's docu for why this is needed
@@ -180,7 +180,7 @@ void WirelessNetworkInterfaceActivatableProvider::handleAdd(Knm::Connection * ad
                                 d->wirelessInterface(), addedConnection, d->interface->uni(), this);
                     d->activatables.insert(addedConnection->uuid().toString(), hiddenWic);
                     d->activatableList->addActivatable(hiddenWic);
-                }
+                }*/
             }
         }
         maintainActivatableForUnconfigured();

plasma_nm_version.h
@@ -1,4 +1,4 @@
 #ifndef PLASMA_NM_VERSION_H
 #define PLASMA_NM_VERSION_H
-static const char * plasmaNmVersion = "nm09-20110929";
+static const char * plasmaNmVersion = "nm09-20110930";
 #endif

applet/activatablelistwidget.cpp
@@ -72,8 +72,8 @@ ActivatableListWidget::ActivatableListWidget(RemoteActivatableList* activatables
 void ActivatableListWidget::init()
 {
     listAppeared();
-    connect(m_activatables, SIGNAL(activatableAdded(RemoteActivatable*)),
-            SLOT(activatableAdded(RemoteActivatable *)));
+    connect(m_activatables, SIGNAL(activatableAdded(RemoteActivatable*, int)),
+            SLOT(activatableAdded(RemoteActivatable *, int)));
     connect(m_activatables, SIGNAL(activatableRemoved(RemoteActivatable*)),
             SLOT(activatableRemoved(RemoteActivatable *)));
 
@@ -164,14 +164,12 @@ bool ActivatableListWidget::accept(RemoteActivatable * activatable) const
     return true;
 }
 
-void ActivatableListWidget::createItem(RemoteActivatable * activatable, const bool addIfAlreadyCached)
+void ActivatableListWidget::createItem(RemoteActivatable * activatable, int index)
 {
     ActivatableItem* ai = m_itemIndex.value(activatable, 0);
     if (ai) {
-        if (addIfAlreadyCached) {
-            m_layout->addItem(ai);
-            ai->show();
-        }
+        m_layout->addItem(ai);
+        ai->show();
         //kDebug() << "activatable already in the layout, not creating an item" << a;
         return;
     }
@@ -215,7 +213,7 @@ void ActivatableListWidget::createItem(RemoteActivatable * activatable, const bo
 
     Q_ASSERT(ai);
     ai->setupItem();
-    m_layout->addItem(ai);
+    m_layout->insertItem(index + 1, ai);
     m_itemIndex[activatable] = ai;
     connect(ai, SIGNAL(disappearAnimationFinished()),
             this, SLOT(deleteItem()));
@@ -241,8 +239,10 @@ void ActivatableListWidget::createHiddenItem()
 
 void ActivatableListWidget::listAppeared()
 {
-    foreach (RemoteActivatable* remote, m_activatables->sortedActivatables()) {
-        activatableAdded(remote);
+    int i = 0;
+    foreach (RemoteActivatable* remote, m_activatables->activatables()) {
+        activatableAdded(remote, i);
+        i++;
     }
     filter();
 }
@@ -250,9 +250,6 @@ void ActivatableListWidget::listAppeared()
 void ActivatableListWidget::deactivateConnection(const QString& deviceUni)
 {
     foreach (ActivatableItem* item, m_itemIndex) {
-        if (!item) { // the item might be gone here
-            continue;
-        }
         RemoteInterfaceConnection *conn = item->interfaceConnection();
         if (conn && conn->deviceUni() == deviceUni) {
             //kDebug() << "deactivating" << conn->connectionName();
@@ -273,11 +270,11 @@ void ActivatableListWidget::listDisappeared()
     m_hiddenItem = 0;
 }
 
-void ActivatableListWidget::activatableAdded(RemoteActivatable * added)
+void ActivatableListWidget::activatableAdded(RemoteActivatable * added, int index)
 {
-    //kDebug();
+    kDebug();
     if (accept(added)) {
-        createItem(added);
+        createItem(added, index);
     }
     if(added->activatableType() == Knm::Activatable::WirelessInterfaceConnection && static_cast<RemoteWirelessInterfaceConnection*>(added)->operationMode() == Solid::Control::WirelessNetworkInterfaceNm09::Adhoc)
         connect(added,SIGNAL(changed()),SLOT(filter()));
@@ -294,22 +291,20 @@ void ActivatableListWidget::filter()
 {
     // Clear connection list first, but do not delete the items.
     foreach (ActivatableItem* item, m_itemIndex) {
-        if (!item) { // the item might be gone here
-            continue;
-        }
-
         // Hide them first to prevent glitches in GUI.
         item->hide();
         m_layout->removeItem(item);
     }
 
-    foreach (RemoteActivatable *act, m_activatables->sortedActivatables()) {
+    int i = 0;
+    foreach (RemoteActivatable *act, m_activatables->activatables()) {
         if (accept(act)) {
             // The "true" parameter means add the item to m_layout if it is already cached in m_itemIndex.
-            createItem(act, true);
+            createItem(act, i);
         } else {
             activatableRemoved(act);
         }
+        i++;
     }
 
     if (!m_interfaces.isEmpty() && m_hasWireless) {
@@ -338,7 +333,7 @@ void ActivatableListWidget::filter()
 
 void ActivatableListWidget::activatableRemoved(RemoteActivatable * removed)
 {
-    ActivatableItem *it = m_itemIndex[removed];
+    ActivatableItem *it = m_itemIndex.value(removed, 0);
     if (!it) {
         return;
     }

applet/activatablelistwidget.h
@@ -51,7 +51,7 @@ Q_SIGNALS:
     void showInterfaceDetails(QString);
 
 public Q_SLOTS:
-    void activatableAdded(RemoteActivatable*);
+    void activatableAdded(RemoteActivatable*, int);
     void activatableRemoved(RemoteActivatable*);
     void listDisappeared();
     void listAppeared();
@@ -70,7 +70,7 @@ private Q_SLOTS:
     void filter();
 
 private:
-    void createItem(RemoteActivatable* conn, const bool addIfAlreadyCached = false);
+    void createItem(RemoteActivatable* conn, int index);
     void createHiddenItem();
     int m_connectionType;
     QList<Knm::Activatable::ActivatableType> m_types;

applet/interfaceitem.cpp
@@ -68,7 +68,7 @@ InterfaceItem::InterfaceItem(Solid::Control::NetworkInterfaceNm09 * iface, Remot
     m_starting(true)
 {
     connect(m_activatables, SIGNAL(disappeared()), this, SLOT(serviceDisappeared()));
-    connect(m_activatables, SIGNAL(activatableAdded(RemoteActivatable*)), SLOT(activatableAdded(RemoteActivatable*)));
+    connect(m_activatables, SIGNAL(activatableAdded(RemoteActivatable*, int)), SLOT(activatableAdded(RemoteActivatable*)));
     connect(m_activatables, SIGNAL(activatableRemoved(RemoteActivatable*)), SLOT(activatableRemoved(RemoteActivatable*)));
     setDrawBackground(true);
     setTextBackgroundColor(QColor(Qt::transparent));

applet/networkmanager.cpp
@@ -111,7 +111,7 @@ NetworkManagerApplet::NetworkManagerApplet(QObject * parent, const QVariantList 
     kded.call(QLatin1String("loadModule"), QLatin1String("networkmanagement"));
 
     m_activatables = new RemoteActivatableList(this);
-    connect(m_activatables, SIGNAL(activatableAdded(RemoteActivatable*)), this, SLOT(activatableAdded(RemoteActivatable*)));
+    connect(m_activatables, SIGNAL(activatableAdded(RemoteActivatable*, int)), this, SLOT(activatableAdded(RemoteActivatable*)));
     connect(m_activatables, SIGNAL(activatableRemoved(RemoteActivatable*)), this, SLOT(activatableRemoved(RemoteActivatable*)));
     connect(m_activatables, SIGNAL(disappeared()), this, SLOT(activatablesDisappeared()));
 }

applet/nmpopup.cpp
@@ -209,7 +209,7 @@ void NMPopup::init()
     m_showMoreButton->setMinimumSize(sMax);
     m_showMoreButton->setMaximumSize(sMax);
     connect(m_showMoreButton, SIGNAL(clicked()), this, SLOT(showMore()));
-    connect(m_activatables, SIGNAL(activatableAdded(RemoteActivatable *)), this, SLOT(uncheckShowMore(RemoteActivatable *)));
+    connect(m_activatables, SIGNAL(activatableAdded(RemoteActivatable *, int)), this, SLOT(uncheckShowMore(RemoteActivatable *)));
     connect(m_activatables, SIGNAL(activatableRemoved(RemoteActivatable *)), this, SLOT(checkShowMore(RemoteActivatable *)));
 
     QGraphicsLinearLayout* connectionLayout = new QGraphicsLinearLayout;

commit 6e58296dba8dab5e1880e5627ccc14ed66a07ae4

fix connecting to a hidden network, broken by commit 53c55be9b7a104975881334459c0f3a64cf1bfb2

libs/internals/activatable.h
@@ -41,11 +41,11 @@ public:
      */
     enum ActivatableType {
         WirelessInterfaceConnection,
-        HiddenWirelessInterfaceConnection,
         VpnInterfaceConnection,
         GsmInterfaceConnection,
         InterfaceConnection,
         WirelessNetwork,
+        HiddenWirelessInterfaceConnection,
         UnconfiguredInterface
     };
 

libs/service/sortedactivatablelist.cpp
@@ -177,13 +177,13 @@ bool activatableLessThan(const Knm::Activatable * first, const Knm::Activatable 
     // debug ends
 #endif
 
-    // order by device
-    int i = compareDevices(first, second);
+    // order by activatable type
+    int i = compareActivatableType(first, second);
 
-    // then by activatable type
-    if (i == 0) {
+    // then by device
+    if (i == 0 && !first->activatableType() != Knm::Activatable::VpnInterfaceConnection && !second->activatableType() != Knm::Activatable::VpnInterfaceConnection) {
 //X         kDebug() << s1 << "and" << s2 << "are on the same device, comparing activatable type";
-        i = compareActivatableType(first, second);
+        i = compareDevices(first, second);
     }
 
     // then by state
@@ -255,11 +255,6 @@ int compareDevices(const Knm::Activatable * first, const Knm::Activatable * seco
         return 0;
     }
 
-    if (first->activatableType() == Knm::Activatable::VpnInterfaceConnection || second->activatableType() == Knm::Activatable::VpnInterfaceConnection || first->activatableType() == Knm::Activatable::WirelessNetwork || second->activatableType() == Knm::Activatable::WirelessNetwork || first->activatableType() == Knm::Activatable::UnconfiguredInterface || second->activatableType() == Knm::Activatable::UnconfiguredInterface) {
-        return 0;
-    }
-
-    // not VPN, therefore use the real logic
     Solid::Control::NetworkInterfaceNm09 * firstIface = Solid::Control::NetworkManagerNm09::findNetworkInterface(first->deviceUni());
     Solid::Control::NetworkInterfaceNm09 * secondIface = Solid::Control::NetworkManagerNm09::findNetworkInterface(second->deviceUni());
 
@@ -267,9 +262,10 @@ int compareDevices(const Knm::Activatable * first, const Knm::Activatable * seco
         if (firstIface->type() == secondIface->type()) {
             return firstIface->interfaceName().compare(secondIface->interfaceName());
         } else {
-            return SortedActivatableList::s_solidTypesToOrder.value(firstIface->type()) - SortedActivatableList::s_solidTypesToOrder.value(secondIface->type());
+            return (int)firstIface->type() - (int)secondIface->type();
         }
     }
+
     return 0;
 }
 

libs/service/wirelessnetworkinterfaceactivatableprovider.cpp
@@ -170,7 +170,7 @@ void WirelessNetworkInterfaceActivatableProvider::handleAdd(Knm::Connection * ad
                     d->activatables.insert(addedConnection->uuid().toString(), ifaceConnection);
                     d->activatableList->addActivatable(ifaceConnection);
                 }
-                /*else if (!ourHiddenWicFound) {
+                else if (!ourHiddenWicFound) {
                     // create a HiddenWirelessInterfaceConnection for this connection as well
                     // this allows adhoc and hidden wireless networks' connections to be activated
                     // see HiddenWIC's docu for why this is needed
@@ -180,7 +180,7 @@ void WirelessNetworkInterfaceActivatableProvider::handleAdd(Knm::Connection * ad
                                 d->wirelessInterface(), addedConnection, d->interface->uni(), this);
                     d->activatables.insert(addedConnection->uuid().toString(), hiddenWic);
                     d->activatableList->addActivatable(hiddenWic);
-                }*/
+                }
             }
         }
         maintainActivatableForUnconfigured();

plasma_nm_version.h
@@ -1,4 +1,4 @@
 #ifndef PLASMA_NM_VERSION_H
 #define PLASMA_NM_VERSION_H
-static const char * plasmaNmVersion = "nm09-20110930";
+static const char * plasmaNmVersion = "nm09-20111001";
 #endif

settings/configshell/manageconnection.cpp
@@ -41,7 +41,7 @@ ManageConnection::ManageConnection(Knm::Connection *con): m_manager("org.kde.net
         foreach(const QString & connectionId, mSystemSettings->connectionList()->connections()) {
             Knm::Connection * c = mSystemSettings->connectionList()->findConnection(connectionId);
             Knm::BluetoothSetting * setting = static_cast<Knm::BluetoothSetting *>(c->setting(Knm::Setting::Bluetooth));
-    
+
             if (setting && setting->bdaddr() == btSetting->bdaddr()) {
                 kDebug() << "Updating existing bluetooth connection instead of creating one";
                 connect(mSystemSettings, SIGNAL(connectionsChanged()), SLOT(updateConnectionCompleted()));
@@ -57,7 +57,7 @@ ManageConnection::ManageConnection(Knm::Connection *con): m_manager("org.kde.net
 
     if (addConnection) {
         connect(mSystemSettings, SIGNAL(addConnectionCompleted(bool, const QString &)), SLOT(addConnectionCompleted(bool, const QString &)));
-        connect(&m_manager, SIGNAL(ActivatableAdded(QString, uint)), this, SLOT(activatableAdded(QString, uint)));
+        connect(&m_manager, SIGNAL(ActivatableAdded(QString, uint, int)), this, SLOT(activatableAdded(QString, uint, int)));
         mSystemSettings->addConnection(con);
     }
 }
@@ -88,8 +88,9 @@ void ManageConnection::addConnectionCompleted(bool valid, const QString &errorMe
     }
 }
 
-void ManageConnection::activatableAdded(QString path, uint type)
+void ManageConnection::activatableAdded(QString path, uint type, int index)
 {
+    Q_UNUSED(index)
     QDBusInterface activatable("org.kde.networkmanagement",
                                path,
                                "org.kde.networkmanagement.Activatable",

settings/configshell/manageconnection.h
@@ -36,7 +36,7 @@ public:
 private Q_SLOTS:
     void addConnectionCompleted(bool valid, const QString &errorMessage);
     void updateConnectionCompleted();
-    void activatableAdded(QString, uint);
+    void activatableAdded(QString, uint, int);
 private:
     NMDBusSettingsConnectionProvider * mSystemSettings;
     QDBusInterface m_manager;