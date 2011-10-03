2b5130205d951d5758049f114cfccc6f05881cdc
Lamarque V. Souza <lamarque@gmail.com>
2011-10-02 22:09:32 -0300

    Set default GSM's "password" flag to AgentOwned to prevent the password from not being saved at all and, consequently, the secrets dialog appearing at each connection attempt.
    BUG: 283204
    FIXED-IN: nm09

--- libs/ui/gsmwidget.cpp
+++ libs/ui/gsmwidget.cpp
@@ -142,7 +142,6 @@ void GsmWidget::setNetworkIds(const QList<QVariant> networkIds)
 void GsmWidget::setApnInfo(const QMap<QString, QVariant> apnInfo)
 {
     Q_D(GsmWidget);
-
     d->setting->setNumber(apnInfo["number"].toString());
 
     if (!apnInfo["username"].isNull()) {
@@ -150,6 +149,7 @@ void GsmWidget::setApnInfo(const QMap<QString, QVariant> apnInfo)
     }
     if (!apnInfo["password"].isNull()) {
         d->setting->setPassword(apnInfo["password"].toString());
+        d->setting->setPasswordflags(Knm::Setting::AgentOwned);
     }
 
     d->setting->setApn(apnInfo["apn"].toString());
