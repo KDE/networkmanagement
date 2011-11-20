0f058f4606c9195ad591955afb714a4e484f0fb9
Lamarque V. Souza <lamarque@kde.org>
2011-11-20 14:13:12 -0200

    Revert "Fix creation of Bluetooth PAN connections." I am going to apply a different implementation in the next commit.
    This reverts commit bcd8ebe500909f50e116f63f146488a03a9bd6e2.

--- libs/internals/connection.cpp
+++ libs/internals/connection.cpp
@@ -384,19 +384,6 @@ void Connection::addSetting(Setting * newSetting)
     m_settings.append(newSetting);
 }
 
-void Connection::removeSetting(Setting::Type type)
-{
-    QMutableListIterator<Setting *> iter(m_settings);
-    while(iter.hasNext()) {
-        Setting * setting = iter.next();
-        if (setting->type() == type) {
-            iter.remove();
-            delete setting;
-            break;
-        }
-    }
-}
-
 bool Connection::hasPersistentSecrets() const
 {
     bool connectionHasSecrets = false;
