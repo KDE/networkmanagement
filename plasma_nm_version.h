99893f40dff550f3bc1e6db93981985053b4ba11
Lamarque V. Souza <lamarque@kde.org>
2011-11-26 11:41:30 -0200

    Increase networkmanagement_configshell timeout from 30s to 60s.

--- plasma_nm_version.h
+++ plasma_nm_version.h
@@ -1,4 +1,4 @@
 #ifndef PLASMA_NM_VERSION_H
 #define PLASMA_NM_VERSION_H
-static const char * plasmaNmVersion = "0.9.0_rc2 (master 20111122)";
+static const char * plasmaNmVersion = "0.9.0_rc2 (master 20111126)";
 #endif
