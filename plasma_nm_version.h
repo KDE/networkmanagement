aa2e2a7f61234f9790d046cfd761e110bb2750a2
Ilia Kats <ilia-kats@gmx.net>
2011-11-30 16:58:35 +0100

    fix setting focus on the first empty input field in vpnc auth dialog
    KDialog::show() is called in connectionsecretsjob.cpp only after readSecrets()
    has been called, so d->ui.userPasswordLabel->isVisible() returns false
    when called in readSecrets()
    (cherry picked from commit bbec5b5ced0d23bf4c4c1ab922824e8f4a673620)

--- plasma_nm_version.h
+++ plasma_nm_version.h
@@ -1,4 +1,4 @@
 #ifndef PLASMA_NM_VERSION_H
 #define PLASMA_NM_VERSION_H
-static const char * plasmaNmVersion = "0.9.1_rc1 (master 20111128)";
+static const char * plasmaNmVersion = "0.9.1_rc1 (master 20111130)";
 #endif
