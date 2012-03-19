b58c105abb9ab61d70b2245302023ebdc1629492
Lamarque V. Souza <lamarque@kde.org>
2012-03-19 20:24:03 -0300

    Check for libnm-util and libnm-glib.

--- plasma_nm_version.h
+++ plasma_nm_version.h
@@ -1,4 +1,4 @@
 #ifndef PLASMA_NM_VERSION_H
 #define PLASMA_NM_VERSION_H
-static const char * plasmaNmVersion = "0.9.0 (nm09 20120306)";
+static const char * plasmaNmVersion = "0.9.0 (nm09 20120319)";
 #endif
