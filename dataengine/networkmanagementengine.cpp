5b16b83fe45af67a45161d6e88152e91c01f9aef
Lukas Tinkl <lukas@kde.org>
2012-06-26 13:02:58 +0200

    fix typo

--- dataengine/networkmanagementengine.cpp
+++ dataengine/networkmanagementengine.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 210 Sebastian Kügler <sebas@kde.org>
+    Copyright 2010 Sebastian Kügler <sebas@kde.org>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
