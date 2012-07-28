075b44eca5ac27cd5f52040a9d00461ca91ce15d
Lamarque V. Souza <lamarque@kde.org>
2012-07-28 20:57:17 -0300

    Change version for release.

--- plasma_nm_version.h
+++ plasma_nm_version.h
@@ -1,4 +1,4 @@
 #ifndef PLASMA_NM_VERSION_H
 #define PLASMA_NM_VERSION_H
-static const char * plasmaNmVersion = "0.9.0.3 (nm09 20120726)";
+static const char * plasmaNmVersion = "0.9.0.4 (nm09 20120728)";
 #endif
