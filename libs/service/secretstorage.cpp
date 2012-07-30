96246a4aa370873dee982846cde543091be086b6
Lamarque V. Souza <lamarque@kde.org>
2012-07-30 16:43:50 -0300

    Add kDebug() calls to debug problem with missing password field with WPA2 enterprise connections.
    CCBUG: 299868

--- libs/service/secretstorage.cpp
+++ libs/service/secretstorage.cpp
@@ -162,6 +162,7 @@ void SecretStorage::walletOpenedForRead(bool success)
                         if (setting8021x) {
                             if (settingSecurity->securityType() == Knm::WirelessSecuritySetting::EnumSecurityType::DynamicWep ||
                                 settingSecurity->securityType() == Knm::WirelessSecuritySetting::EnumSecurityType::WpaEap) {
+                                kDebug() << "Enabling workaround for DynamicWep and WpaEap";
                                 setting8021x->setEnabled(true); // needed for needSecrets() below, otherwise needSecrets() returns an empty list.
                             }
                         }
@@ -181,6 +182,7 @@ void SecretStorage::walletOpenedForRead(bool success)
                                 setting->secretsFromMap(map);
                             }
                             QStringList needSecretsList = setting->needSecrets();
+                            kDebug() << "Needed secrets" << needSecretsList;
                             if ((pair.second & RequestNew) || (!needSecretsList.isEmpty() && (pair.second & AllowInteraction))) {
                                 askUser(con, pair.first, needSecretsList);
                             } else {
