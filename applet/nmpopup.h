3bb65592de1af385467ff21d88e030d7b3b24b67
Lamarque V. Souza <lamarque@kde.org>
2012-09-08 18:37:07 -0300

    Update some copyright headers.

--- applet/nmpopup.h
+++ applet/nmpopup.h
@@ -1,6 +1,6 @@
 /*
 Copyright 2009 Sebastian Kügler <sebas@kde.org>
-Copyright 2011 Lamarque Souza <lamarque@kde.org>
+Copyright 2011-2012 Lamarque V. Souza <lamarque@kde.org>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License as
