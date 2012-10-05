ff2cb614562a4be5ac23c7e765c15a59d8273c16
Lamarque V. Souza <lamarque@kde.org>
2012-10-05 16:29:28 -0300

    Update copyright header.

--- libs/ui/connectionsecretsjob.cpp
+++ libs/ui/connectionsecretsjob.cpp
@@ -1,5 +1,6 @@
 /*
 Copyright 2008 Will Stephenson <wstephenson@kde.org>
+Copyright 2011-2012 Lamarque V. Souza <lamarque@kde.org>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License as
