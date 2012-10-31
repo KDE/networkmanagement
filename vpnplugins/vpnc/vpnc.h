63b90e694694cbd2056c76779a4ce12b6cf5b615
Rajeesh K Nambiar <rajeeshknambiar@gmail.com>
2012-10-31 06:11:43 -0200

    CISCO vpn pcf files can have string keys either as "<key>" or "!<key>". If <key> is not found, look for !<key> too.
    BUG: 309262
    FIXED-IN: 0.9.0.6

--- vpnplugins/vpnc/vpnc.h
+++ vpnplugins/vpnc/vpnc.h
@@ -1,6 +1,7 @@
 /*
 Copyright 2008 Will Stephenson <wstephenson@kde.org>
-Copyright 2011 Rajeesh K Nambiar <rajeeshknambiar@gmail.com>
+Copyright 2011-2012 Rajeesh K Nambiar <rajeeshknambiar@gmail.com>
+Copyright 2011-2012 Lamarque V. Souza <lamarque@kde.org>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License as
@@ -40,6 +41,7 @@ public:
     QString decryptedPasswd;
     VpncUiPluginPrivate();
     ~VpncUiPluginPrivate();
+    QString readStringKeyValue(const KConfigGroup & configGroup, const QString & key);
 protected Q_SLOTS:
     void gotciscoDecryptOutput();
     void ciscoDecryptError(QProcess::ProcessError pError);
