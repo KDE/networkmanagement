d7bba3790641e9c29ab665f97db03135b668bb70
Lamarque V. Souza <lamarque@kde.org>
2012-12-01 12:36:50 -0200

    Add MD5 and GTC options to WPA2 Enterprise's TTLS setting dialog.
    FIXED-IN: 0.9.0.6
    (cherry picked from commit dc25d9864fc309789ba23b668913873df1c100d7)
    
    Conflicts:
    
    	plasma_nm_version.h

--- plasma_nm_version.h
+++ plasma_nm_version.h
@@ -1,4 +1,4 @@
 #ifndef PLASMA_NM_VERSION_H
 #define PLASMA_NM_VERSION_H
-static const char * plasmaNmVersion = "0.9.0.5 (nm09 20121031)";
+static const char * plasmaNmVersion = "0.9.0.5 (nm09 20121201)";
 #endif
