bea294c0deec6c208177ce507aa87cb8699a3500
Lamarque V. Souza <lamarque@kde.org>
2012-12-26 19:04:24 -0200

    Change version string. Note: the code in this branch is never going to be released. Use master branch if you want new features.

--- plasma_nm_version.h
+++ plasma_nm_version.h
@@ -1,4 +1,4 @@
 #ifndef PLASMA_NM_VERSION_H
 #define PLASMA_NM_VERSION_H
-static const char * plasmaNmVersion = "0.9.1_rc1 (master 20121206)";
+static const char * plasmaNmVersion = "0.9.1_rc1 (NM/0.9.1 20121226)";
 #endif
