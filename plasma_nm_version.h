507bfe153761e6bcb39b67cd9118b95dd015da41
Lamarque V. Souza <lamarque@kde.org>
2013-01-02 11:57:53 -0200

    Remove unused variable.

--- plasma_nm_version.h
+++ plasma_nm_version.h
@@ -1,4 +1,4 @@
 #ifndef PLASMA_NM_VERSION_H
 #define PLASMA_NM_VERSION_H
-static const char * plasmaNmVersion = "0.9.1.70 (master 20121226)";
+static const char * plasmaNmVersion = "0.9.1.70 (master 20130102)";
 #endif
