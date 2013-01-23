9d5ed31547383ca8d218d7cebe58824ebcf06213
Lamarque V. Souza <lamarque@kde.org>
2013-01-22 23:21:13 -0200

    Initialize variable here to fix Metric edit box in VPN's routes window.
    BUG: 311837
    FIXED-IN: 0.9.0.7

--- plasma_nm_version.h
+++ plasma_nm_version.h
@@ -1,4 +1,4 @@
 #ifndef PLASMA_NM_VERSION_H
 #define PLASMA_NM_VERSION_H
-static const char * plasmaNmVersion = "0.9.0.6 (nm09 20130121)";
+static const char * plasmaNmVersion = "0.9.0.6 (nm09 20130122)";
 #endif
